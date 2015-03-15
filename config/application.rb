d771d61a3cebfff9dd78dd8614e5b4ef9ec30669
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-03-14 19:15:50 -0700

    Configure asset paths so that large amounts of text are not needed to reference bootstrap stylesheets; import bootstrap in main.scss

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module RailsAngular
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.paths << Rails.root.join("lib","assets","bower_components","bootstrap-sass-official", "assets", "stylesheets")
+    config.assets.paths << Rails.root.join("lib","assets","bower_components","bootstrap-sass-official", "assets","fonts")
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
