110d6c8eafdc65082fbe049be1026983c46ed45c
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-03-17 23:14:40 -0700

    Add abstract articles state to namespace articles, as well as nested states for example article views; Also uncomment the html5mode to clean up the urls

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -1,7 +1,8 @@
-angular.module('myApp', [
-  'ngAnimate',
-  'ui.router',
-  'templates'
+angular
+  .module('myApp', [
+    'ngAnimate',
+    'ui.router',
+    'templates'
   ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     /**
@@ -12,12 +13,33 @@ angular.module('myApp', [
         url: '/',
         templateUrl: 'home.html',
         controller: 'HomeCtrl'
-      });
+      })
+
+      .state('articles', {
+        abstract: true,
+        url: '/articles',
+        templateUrl: 'articles/layout.html'
+      })
+
+        .state('articles.one', {
+          url: '',
+          templateUrl: 'articles/one.html'
+        })
+
+        .state('articles.two', {
+          url: '/two',
+          templateUrl: 'articles/two.html'
+        })
+
+        .state('articles.three', {
+          url: '/three',
+          templateUrl: 'articles/three.html'
+        });
 
     // default fall back route
     $urlRouterProvider.otherwise('/');
 
     // enable HTML5 mode for SEO
-    // $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
   });
